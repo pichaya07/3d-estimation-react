d769d149b6c90d576b041e086646ae6c080aeb0b
Pichaya <pichpichaya7@gmail.com>
2022-12-13 23:01:13 +0700

    Setup react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { StlViewer } from "react-stl-viewer"
 
+const url = "https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl"
+
+const style = {
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StlViewer
+      style={style}
+      orbitControls
+      shadows
+      url={url}
+    />
   );
 }
 
